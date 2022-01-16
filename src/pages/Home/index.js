5a4032aea46bc4fd6a1f138b0f5a996450655d20
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-01-16 00:11:49 -0300

    projeto 1 udemy

---
+++ src/pages/Home/index.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
+
+export default function Home() {
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+
+        async function loadFilmes(){
+            const response = await api.get('r-api/?api=filmes')
+            setFilmes(response.data);
+        }
+
+        loadFilmes();
+
+    }, []);
+
+    return (
+        <div>
+            <h1>Pagina Home</h1>
+        </div>
+    );
+}
