276c56ea369402bab2856ebc0a8f9b97a4998899
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-01-17 20:55:13 -0300

    fim

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './filme-info.css';
 import { useParams, useHistory } from 'react-router-dom';
 import api from '../../services/api';
+import { toast } from "react-toastify";
 
 export default function Filme(){
     const {id} = useParams();
@@ -30,6 +31,27 @@ export default function Filme(){
 
     }, [history, id]);
 
+    function salvaFilme(){
+
+        const minhaLista = localStorage.getItem('filmes');
+
+        let filmesSalvos = JSON.parse(minhaLista) || [];
+
+        //Se tiver algum filme salvo, esse mesmo ID precisa ignorar...
+
+        const hasFilme = filmesSalvos.some( (filmesSalvo) => filmesSalvo.id === filme.id)
+
+        if(hasFilme){
+            toast.error('Você já possui esse filme salvo.');
+            return;
+            //Para execução do código aqui...
+        }
+
+        filmesSalvos.push(filme);
+        localStorage.setItem('filmes', JSON.stringify(filmesSalvos));
+        toast.success('Filme salvo com sucesso!');
+    }
+
     if(loading){
         return(
             <div className='filme-info'>
@@ -46,7 +68,7 @@ export default function Filme(){
             {filme.sinopse}
 
             <div className='botoes'>
-                <button onClick={()=>{}}>Salvar</button>
+                <button onClick={salvaFilme}>Salvar</button>
                 <button>
                     <a target="_blank" href={`https://youtube.com/results?search_query=${filme.nome} Trailer`}>
                         Trailer
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,9 @@
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-
 import Home from './pages/Home';
-
 import Header from './components/Header';
-
 import Filme from './pages/Filme';
+import Favoritos from './pages/Favoritos';
+import Erro from './pages/Erro';
 
 const Routes = () => {
     return(
@@ -13,6 +12,8 @@ const Routes = () => {
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/filme/:id" component={Filme} />
+                <Route exact path="/favoritos" component={Favoritos} />
+                <Route path="*" component={Erro} />
             </Switch>
         </BrowserRouter>
     )
