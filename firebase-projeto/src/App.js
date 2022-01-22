e22682b08b22cf611e5244c7c43188e6e2d94b55
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-01-22 17:13:11 -0300

    fim firebase

--- firebase-projeto/src/App.js
+++ firebase-projeto/src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [senha, setSenha] = useState('');
   const [cargo, setCargo] = useState('');
   const [nome, setNome] = useState('');
+  const [user, setUser] = useState({});
 
 
   async function novoUsuario(){
@@ -47,6 +48,30 @@ function App() {
 
   async function logout(){
     await firebase.auth().signOut();
+    setUser({});
+  }
+
+  async function login(){
+    await firebase.auth().signInWithEmailAndPassword(email, senha)
+    .then(async(value)=>{
+      await firebase.firestore().collection('users')
+      .doc(value.user.uid)
+      .get()
+      .then((snapshot)=>{
+        setUser({
+        nome: snapshot.data().nome,
+        cargo: snapshot.data().cargo,
+        status: snapshot.data().status,
+        email: value.user.email
+      });
+
+    })
+
+  })
+  .catch((error)=>{
+    console.log('ERRO AO LOGAR' + error)
+  })
+
   }
 
   return (
@@ -64,11 +89,23 @@ function App() {
       <input type="text" value={email} onChange={ (e) => setEmail(e.target.value) } /> <br/>
 
       <label>Senha</label>
-      <input type="password" value={senha} onChange={ (e) => setSenha(e.target.value) } /> <br/> 
+      <input type="password" value={senha} onChange={ (e) => setSenha(e.target.value) } /> <br/>
 
+      <button onClick={ login }>Fazer Login</button>
       <button onClick={ novoUsuario }>Cadastrar</button>
       <button onClick={ logout } >Sair da conta!</button> 
     </div>
+
+    <hr/> <br/>
+
+    {Object.keys(user).length > 0 && (
+      <div>
+        <strong>Olá </strong> {user.nome} <br/>
+        <strong>Cargo: </strong> {user.cargo} <br/>
+        <strong>Email: </strong> {user.email} <br/>
+        <strong>Status: </strong> {user.status ? 'ATIVO' : 'DESATIVADO'} <br/>
+      </div>
+    )}
 
     </div>
   );
