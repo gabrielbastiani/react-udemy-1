36a1a67954d00ca5af6cff26a8f776b6427b723c
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-01-23 20:46:07 -0300

    projeto

--- cadastro-de-usuario/src/App.js
+++ cadastro-de-usuario/src/App.js
@@ -1,8 +1,13 @@
 
+import {BrowserRouter} from 'react-router-dom';
+import Routes from './routes';
+
 function App() {
   return (
     <div>
-     
+      <BrowserRouter>
+        <Routes/>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ cadastro-de-usuario/src/services/firebaseConnection.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCRs9JCXHUTBT6TCDPK-l_ofrWKpjCmpCk",
+    authDomain: "sistema-de-cadastro-bd2ae.firebaseapp.com",
+    projectId: "sistema-de-cadastro-bd2ae",
+    storageBucket: "sistema-de-cadastro-bd2ae.appspot.com",
+    messagingSenderId: "900333506272",
+    appId: "1:900333506272:web:46ad8e6b780c26786e0a85",
+    measurementId: "G-9EW7G1HR6Z"
+  };
+  
+  if(!firebase.apps.length){
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  }
+
+  export default firebase;
