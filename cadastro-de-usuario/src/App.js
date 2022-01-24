5c7e9813488ed19b9036aeaa27c5fb35093cad73
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-01-24 20:59:45 -0300

    cadastro de usuario

--- cadastro-de-usuario/src/App.js
+++ cadastro-de-usuario/src/App.js
@@ -1,14 +1,15 @@
 
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from './contexts/auth';
 import Routes from './routes';
 
 function App() {
   return (
-    <div>
+    <AuthProvider>
       <BrowserRouter>
         <Routes/>
       </BrowserRouter>
-    </div>
+    </AuthProvider>
   );
 }
 
--- cadastro-de-usuario/src/pages/Dashboard/index.js
+++ cadastro-de-usuario/src/pages/Dashboard/index.js
@@ -1,7 +1,15 @@
+
+import { useContext } from 'react';
+
+import { AuthContext } from '../../contexts/auth';
+
 export default function Dashboard(){
-    return(
-        <div>
-        <h1>Página Dashboard</h1>
-        </div>
-    )
+  const { signOut } = useContext(AuthContext);
+
+  return(
+    <div>
+      <h1>PAGINA DASHBOARD</h1>
+      <button onClick={ () => signOut() }>Fazer logout</button>
+    </div>
+  )
 }
