6f99b6065b900afe46f38fedf01df55c4b373ce9
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-01-29 16:59:14 -0300

    modal

--- cadastro-de-usuario/src/pages/Customers/index.js
+++ cadastro-de-usuario/src/pages/Customers/index.js
@@ -4,66 +4,70 @@ import './customers.css';
 import Title from '../../components/Title';
 import Header from '../../components/Header';
 import firebase from '../../services/firebaseConnection';
-import {FiUser} from 'react-icons/fi';
+import { FiUser } from 'react-icons/fi';
 
 import { toast } from 'react-toastify';
 
 export default function Customers(){
-    const [nomeFantasia, setNomeFantasia] = useState('');
-    const [cnpj, setCnpj] = useState('');
-    const [endereco, setEndereco] = useState('');
+  const [nomeFantasia, setNomeFantasia] = useState('');
+  const [cnpj, setCnpj] = useState('');
+  const [endereco, setEndereco] = useState('');
 
-    async function handleAdd(e){
-        e.preventDefault();
 
-        if(nomeFantasia !== '' && cnpj !== '' && endereco !== ''){
-            await firebase.firestore().collection('customers')
-            .add({
-                nomeFantasia: nomeFantasia,
-                cnpj: cnpj,
-                endereco: endereco
-            })
-            .then(()=>{
-                setNomeFantasia('');
-                setCnpj('');
-                setEndereco('');
-                toast.info('Empresa cadastrada com sucesso!');
-            })
-            .catch((error)=>{
-                console.log(error);
-                toast.error('Erro ao cadastrar essa empresa.');
-            })
-        }else{
-            toast.error('Preencha todos os campos!');
-        }
 
+  async function handleAdd(e){
+    e.preventDefault();
+    
+    if(nomeFantasia !== '' && cnpj !== '' && endereco !== ''){
+      await firebase.firestore().collection('customers')
+      .add({
+        nomeFantasia: nomeFantasia,
+        cnpj: cnpj,
+        endereco: endereco
+      })
+      .then(()=>{
+        setNomeFantasia('');
+        setCnpj('');
+        setEndereco('');
+        toast.info('Empresa cadastrada com sucesso!');
+      })
+      .catch((error)=>{
+        console.log(error);
+        toast.error('Erro ao cadastrar essa empresa.');
+      })
+    }else{
+      toast.error('Preencha todos os campos!')
     }
 
-    return(
-        <div>
-            <Header/>
+  }
 
-            <div className='content'>
-                <Title name="Clientes">
-                    <FiUser size={25} />
-                </Title>
+  return(
+    <div>
+      <Header/>
 
-                <div className='container'>
-                    <form className='form-profile customers' onSubmit={handleAdd}>
-                        <label>Nome Fantasia</label>
-                        <input type="text" placeholder="Nome da sua empresa" value={nomeFantasia} onChange={ (e) => setNomeFantasia(e.target.value) } />
+    <div className="content">
+      <Title name="Clientes">
+        <FiUser size={25} />
+      </Title>
 
-                        <label>CNPJ</label>
-                        <input type="text" placeholder="Seu CNPJ" value={cnpj} onChange={ (e) => setCnpj(e.target.value) } />
+      <div className="container">
+        <form className="form-profile customers" onSubmit={handleAdd}>
+          <label>Nome fantasia</label>
+          <input type="text" placeholder="Nome da sua empresa" value={nomeFantasia} onChange={ (e) => setNomeFantasia(e.target.value) } />
 
-                        <label>Endereço</label>
-                        <input type="text" placeholder="Endereço da empresa" value={endereco} onChange={ (e) => setEndereco(e.target.value) } />
+          <label>CNPJ</label>
+          <input type="text" placeholder="Seu CNPJ" value={cnpj} onChange={ (e) => setCnpj(e.target.value) } />
 
-                        <button type="submit">Cadastrar</button>
+          <label>Endereço</label>
+          <input type="text" placeholder="Endereço da empresa" value={endereco} onChange={ (e) => setEndereco(e.target.value) } />
 
-                    </form>
-                </div>
-            </div>
-        </div>
-    )
+          <button type="submit">Cadastrar</button>
+
+        </form>
+      </div>
+
+    </div>
+
+    </div>
+  )
 }
