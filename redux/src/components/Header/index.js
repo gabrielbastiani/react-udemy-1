3af9ef24dd85535130c2b695e3f002b5ee2f2438
gabriel.bastiani <gabriel.bastiani@hotmail.com.br>
2022-02-12 18:29:50 -0300

    redux

--- redux/src/components/Header/index.js
+++ redux/src/components/Header/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import './style.css';
 
 export default function Header() {
- return (
-   <header>
-       <h1>Header</h1>
-   </header>
- );
+    return (
+        <header className = 'container'>
+            <Link to="/">
+                <img className='logo' src={logo} alt='Logo Projeto' />
+            </Link>
+            
+            <Link className='reserva' to="/reservas">
+                <strong>Minhas Reservas</strong>
+                <span>0 reservas</span>
+            </Link>
+        </header>
+    );
 }
--- redux/src/pages/Home/index.js
+++ redux/src/pages/Home/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
  return (
    <div>
-       <h1>ddddddddddddddddd</h1>
+       <h1>Reservas</h1>
    </div>
  );
 }
