59c82047c7b83fc94e2a48239d76e28cf5f2a24a
gabriel.bastiani <gabriel.bastiani@hotmail.com.br>
2022-02-19 10:39:47 -0300

    redux

--- redux/src/store/modules/reserve/actions.js
+++ redux/src/store/modules/reserve/actions.js
@@ -1,6 +1,13 @@
-export function addReserve(trip){
+export function addReserveRequest(id){
   return {
-    type: 'ADD_RESERVE',
+    type: 'ADD_RESERVE_REQUEST',
+    id
+  }
+}
+
+export function addReserveSuccess(trip){
+  return {
+    type: 'ADD_RESERVE_SUCCESS',
     trip
   }
 }
@@ -13,9 +20,9 @@ export function removeReserve(id){
 }
 
 export function updateAmountReserve(id, amount){
-    return {
-        type: 'UPDATE_RESERVE',
-        id,
-        amount
-    }
+  return {
+    type: 'UPDATE_RESERVE',
+    id,
+    amount
+  }
 }
