719650d031a884107a754dae6215a7c3d2caf0fc
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-02-19 13:09:13 -0300

    react

--- redux/src/pages/Home/index.js
+++ redux/src/pages/Home/index.js
@@ -6,7 +6,7 @@ import { MdFlightTakeoff } from 'react-icons/md'
 import api from '../../services/api';
 import './style.css';
 
-export default function Home() {
+export default function Home(history) {
  const dispatch = useDispatch();   
  const [trips, setTrips] = useState([]);
 
@@ -24,6 +24,9 @@ export default function Home() {
 
  function handleAdd(id){
     dispatch(addReserveRequest(id));
+
+    history.push('/reservas');
+    
  }
 
  return (
